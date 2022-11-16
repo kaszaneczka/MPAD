90682b74f197baf623659a7a6ecf1ee45dc72bd8
kudik_lap <kudik233@gmail.com>
2022-11-16 12:40:04 +0100

    7 zad

---
+++ zad7.py
@@ -0,0 +1,27 @@
+import warnings
+
+import arviz as az
+import pymc3 as pm
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
+
+
+
+occurrences = np.concatenate((np.zeros(70), np.ones(30)))
+
+if __name__ == '__main__':
+    with pm.Model() as model:
+        p_d = pm.Uniform("p_d", 0, 1)
+        p_observed = pm.Deterministic("p_observed", 0.25 + 0.5 * p_d)
+        # answers = pm.Bernoulli("answers", p_observed, observed=occurrences)
+        yes_count = 70
+        answers1 = pm.Binomial("answers1", 100, p_observed, observed=yes_count)
+        idata = pm.sample(2000, tune=2500)
+    az.plot_trace(idata, show=True)
+
+    #zad 8 punkt a generuje za pomoca randomchose 100 probek dla ktorych mamy szanse na tak 0,1 lub 0,5 lub 0.9 i sprawdzam
+    #w wygenerowanej tablicy ile wygenerowało sie danych przykładów
+
+    #pkt b to samo co w a tylko dla sznsy = 0.1 i robie to 50 razy zliczajac ile w każdym razie było yesów
